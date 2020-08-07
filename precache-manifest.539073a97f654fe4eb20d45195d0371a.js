c99be3d576cb50bb55acaf73da18e8ad557841ea
oswalth <oswalth2@gmail.com>
2020-08-07 21:12:33 +0300

    Updates

---
+++ precache-manifest.539073a97f654fe4eb20d45195d0371a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3284fa9737c104392907d2259b7c98ca",
+    "url": "/covide-app/index.html"
+  },
+  {
+    "revision": "9ff6f52f209289b2c751",
+    "url": "/covide-app/static/css/main.aa371b85.chunk.css"
+  },
+  {
+    "revision": "6ec2daede1992692755b",
+    "url": "/covide-app/static/js/2.65f50030.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covide-app/static/js/2.65f50030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff6f52f209289b2c751",
+    "url": "/covide-app/static/js/main.9230f2bb.chunk.js"
+  },
+  {
+    "revision": "327808177018cd6b0559",
+    "url": "/covide-app/static/js/runtime-main.bed6aa91.js"
+  }
+]);
